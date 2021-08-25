ae5e155bcb2d0be4252565ea4d5ff8bb6d2b55fb
Angus Gibson <angus@agibson.me>
2021-08-25 18:56:05 +1000

    Update sponge idamp generation script to be dynamic
    We can configure a pretty general sponge inverse damping with this
    script: the timescale, width, and whether to ramp linearly across the
    sponge region

--- scripts/generate_idamp_netcdf4.py
+++ scripts/generate_idamp_netcdf4.py
@@ -1,38 +1,73 @@
 import xarray as xr
 import numpy as np
 import netCDF4
+import argparse
 from datetime import timedelta
+from pathlib import Path
 
-de = xr.open_dataset("/g/data/x77/ahg157/inputs/mom6/panan/vcoord.nc")
-di = xr.open_dataset(
-    "/data/panan/temp_salt_init_z_0025.nc",
-    chunks={"depth": 11, "nyp": 169, "nxp": 360}
+input_dir = "/g/data/x77/ahg157/inputs/mom6/panan"
+
+parser = argparse.ArgumentParser(description="Generate a sponge idamp file")
+parser.add_argument("-i", "--input-dir",
+                    default=input_dir,
+                    type=Path,
+                    help="the directory containing experiment inputs")
+parser.add_argument("-o", "--output-path",
+                    required=True,
+                    type=Path,
+                    help="the output file path, relative to INPUT_DIR")
+parser.add_argument("-w", "--width",
+                    required=True,
+                    type=float,
+                    help="sponge width (in degrees)")
+parser.add_argument("-l", "--linear",
+                    action="store_true",
+                    help="if true, ramp linearly over the sponge width")
+parser.add_argument("--seconds",
+                    default=0,
+                    type=int,
+                    help="sponge timescale (in seconds)")
+parser.add_argument("--hours",
+                    default=0,
+                    type=int,
+                    help="sponge timescale (in hours)")
+parser.add_argument("--days",
+                    default=0,
+                    type=int,
+                    help="sponge timescale (in days)")
+args = parser.parse_args()
+
+dg = xr.open_dataset(args.input_dir / "ocean_hgrid_0025.nc")
+
+sponge_timescale = (
+    timedelta(
+        seconds=args.seconds,
+        hours=args.hours,
+        days=args.days
+    ).total_seconds()
 )
-df = xr.open_dataset("/g/data/x77/ahg157/inputs/mom6/panan/forcing_obc_converted.nc")
-dg = xr.open_dataset("/g/data/x77/ahg157/inputs/mom6//panan/ocean_hgrid_0025.nc")
 
-velocity_timescale = timedelta(days=1).total_seconds()
-
+# hardcode chunk sizes...
 c2d = (169, 360)
 c3d = (11, 169, 360)
 
-kwargs = {
-    "zlib": True,
-    "chunksizes": c3d,
-    "fill_value": netCDF4.default_fillvals["f8"],
-}
-
-do = netCDF4.Dataset("/g/data/x77/ahg157/inputs/mom6/panan/sponge_idamp_velonly.nc", "w")
+do = netCDF4.Dataset(str(args.input_dir / args.output_path), "w")
+# hardcode dimension sizes...
 do.createDimension("nxp", 7200)
 do.createDimension("nyp", 1690)
 do.createDimension("zl", 75)
 do.createDimension("zi", 76)
 
 do.createVariable("Idamp", "f8", ("nyp", "nxp"), zlib=True, chunksizes=c2d, fill_value=netCDF4.default_fillvals["f8"])[:] = 0
-do.variables["Idamp"][:] = (1 / 3600) * (dg.y.isel(nyp=slice(1, None, 2), nxp=slice(1, None, 2)) > -38)
 
-# h = df.dz_u_segment_001.isel(time=0, ny_segment_001=0, nx_segment_001=slice(1, None, 2))
-# do.createVariable("uv_thickness", "f8", ("zl", "nyp", "nxp"), **kwargs)
-# do.variables["uv_thickness"][:] = h.values[:,None,:]
+y = dg.y.isel(nyp=slice(1, None, 2), nxp=slice(1, None, 2))
+north_lat = y.isel(nyp=-1).item(0)
+south_lat = north_lat - args.width
+sponge_region = y > south_lat
+
+if args.linear:
+    do.variables["Idamp"][:] = sponge_region * (((y - south_lat) / args.width) / sponge_timescale)
+else:
+    do.variables["Idamp"][:] = sponge_region * (1 / sponge_timescale)
 
 do.close()
