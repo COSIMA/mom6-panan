a0dd376ccc5da805928e5d5e8e7231541d4924e3
Andy Hogg <Andy.Hogg@anu.edu.au>
2022-01-17 14:03:54 +1100

    adding sync script to git log

---
+++ sync_output_to_gdata.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -q normalbw
+#PBS -l ncpus=1
+#PBS -l wd
+#PBS -l storage=gdata/x77+scratch/x77+gdata/ik11
+#PBS -l walltime=1:00:00,mem=7GB
+#PBS -P x77
+#PBS -N output_to_gdata
+
+# Set this directory to something in /g/data3/hh5/tmp/cosima/
+# Make a unique path for your set of runs.
+# DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
+GDATADIR=/g/data/ik11/outputs/mom6-panan/panant-01-hycom1-v7
+
+mkdir -p ${GDATADIR}
+cd archive
+rsync -vrltoD --safe-links output* ${GDATADIR}
+rsync -vrltoD --safe-links error_logs ${GDATADIR}
+rsync -vrltoD --safe-links pbs_logs ${GDATADIR}
+
+# Be careful here!
+#rsync -vrltoD --safe-links restart* ${GDATADIR}
