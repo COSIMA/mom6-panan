f9d59416bdf2045327f1e50e09e117a310b1777a
Andy Hogg <Andy.Hogg@anu.edu.au>
2022-02-10 09:14:26 +1100

    Preparing for new zstar-v13 case

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -2,7 +2,7 @@
 #PBS -q normalbw
 #PBS -l ncpus=1
 #PBS -l wd
-#PBS -l storage=gdata/x77+scratch/x77
+#PBS -l storage=gdata/x77+scratch/x77+gdata/ik11
 #PBS -l walltime=1:00:00,mem=7GB
 #PBS -P x77
 #PBS -N output_to_gdata
@@ -10,7 +10,7 @@
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # Make a unique path for your set of runs.
 # DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
-GDATADIR=/g/data/x77/amh157/PanAnt/archive/panant-01-zstar
+GDATADIR=/g/data/ik11/outputs/mom6-panan/panant-01-zstar-v13
 
 mkdir -p ${GDATADIR}
 cd archive
